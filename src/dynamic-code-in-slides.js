5cb6da9a36084e487a143e6bec9be3b3b8c3c2c3
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-11-09 11:28:50 +0100

    --wip--

--- src/dynamic-code-in-slides.js
+++ src/dynamic-code-in-slides.js
@@ -1,25 +1,25 @@
-xtab.on('code-update', function (data) {
+const channel = new BroadcastChannel('code-update')
 
-    if (data.id === 'jwt-body') {
+channel.addEventListener('message', function ({ data: { body, secret } }) {
 
-        var object = JSON.parse(data.value);
-        var payload = JSON.stringify(object, null, '    ')
-            .replace(/\[([\s\S]+)\]/mg, (match) => {
-                return match
-                    .replace(/    /g, '')
-                    .replace(/[\n]/g, ' ')
-                    .replace(/\[ /g, '[')
-                    .replace(/ \]/g, ']');
-            });
+    var object = JSON.parse(body);
+    var payload = JSON.stringify(object, null, '    ')
+      .replace(/\[([\s\S]+)\]/mg, (match) => {
+          return match
+            .replace(/    /g, '')
+            .replace(/[\n]/g, ' ')
+            .replace(/\[ /g, '[')
+            .replace(/ \]/g, ']');
+      });
 
-        document.querySelector('[data-code-id="jwt-body"]').innerText = payload;
+    document.querySelector('[data-code-id="jwt-body"]').innerText = payload;
+    document.querySelector('[data-code-id="jwt-secret"]').innerText = secret;
 
-        var jwtHeader = '{"alg": "HS256","typ": "JWT"}';
+    var jwtHeader = '{"alg": "HS256","typ": "JWT"}';
 
-        var jwt = window.sign('HS256', jwtHeader, payload, 's3cr3t');
-        var signature = jwt.result.split('.')[2];
+    var jwt = window.sign('HS256', jwtHeader, payload, secret);
+    var signature = jwt.result.split('.')[2];
 
-        document.querySelector('[data-code-id="jwt-signature"]').innerText = signature;
-    }
+    document.querySelector('[data-code-id="jwt-signature"]').innerText = signature;
 
 });
--- src/editor.js
+++ src/editor.js
@@ -1,7 +1,12 @@
+const channel = new BroadcastChannel('code-update')
+
+const jwtBody = document.querySelector('[data-code-id="jwt-body"]')
+const jwtSecret = document.querySelector('[data-code-id="jwt-secret"]')
+
 document.body.addEventListener('keyup', function (ev) {
 
-    xtab.emit('code-update', {
-        id: ev.target.dataset.codeId,
-        value: ev.target.value,
+    channel.postMessage({
+        body: jwtBody.value,
+        secret: jwtSecret.value
     });
 });
